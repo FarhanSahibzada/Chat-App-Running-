152fa460c81551f9aa567bb377e4d5da7eb239eb
Farhan Sahibzada <farhansahabzada3@gmail.com>
2024-09-03 04:15:39 +0500

    work In Progresss

---
+++ src/component/firebase/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHnWJ4_nC7iE0ugamhOweGDX1uX1Kii94",
+  authDomain: "chat-app-746a8.firebaseapp.com",
+  projectId: "chat-app-746a8",
+  storageBucket: "chat-app-746a8.appspot.com",
+  messagingSenderId: "462684705416",
+  appId: "1:462684705416:web:fd4317eb353497a79ac53d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app)
+
+export {
+  auth,
+  db,
+}
