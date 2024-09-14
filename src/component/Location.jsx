f4e8ec37af4bcca833a95ba021501d6d04ca4457
Farhan Sahibzada <farhansahabzada3@gmail.com>
2024-09-15 04:50:56 +0500

    Update

--- src/component/Location.jsx
+++ src/component/Location.jsx
@@ -30,15 +30,20 @@ function Location() {
     }, [])
 
     return (
-        <LoadScript googleMapsApiKey="AIzaSyCduUlfEdipLTGF-sCizDO488Xm8gExsAA">
-            <GoogleMap
-                mapContainerStyle={{ height: "70vh", width: "100vw" }}
-                center={userLocation}
-                zoom={12}
-            >
-                {!loading && <Marker position={userLocation} label="You are here" />}
-            </GoogleMap>
-        </LoadScript>
+        <div className=' overflow-hidden'>
+            <h1 className='bg-red-600 p-4 text-white font-bold text-xl  fixed top-0 w-full z-50'>My Location</h1>
+            <div className='p-4 mt-14 grid place-items-center bg-base-300 '>
+            <LoadScript googleMapsApiKey="AIzaSyCduUlfEdipLTGF-sCizDO488Xm8gExsAA">
+                <GoogleMap
+                    mapContainerStyle={{ height: "100vh", width: "90vw", borderWidth:4 , borderColor : "red", borderRadius : "20px",}}
+                    center={userLocation}
+                    zoom={12}
+                    >
+                    {!loading && <Marker position={userLocation} label="You are here" />}
+                </GoogleMap>
+            </LoadScript>
+                    </div>
+        </div>
     );
 }
 
